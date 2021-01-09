1159856b9e6cbfd099c6fcd7da6086e1168e2e7f
exekiel87 <ezequiel.saldania>
2021-01-09 17:41:38 -0300

    index quitado

--- migrations/00000000002-patientsIndex.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    up: (db) => {
-        return db.collection('patients').createIndex({dna:1},{unique:1, name:'dna_unique'})
-    },
-    down: (db) => {
-        return db.collection('patients').dropIndex('dna_unique');
-    }
-};
